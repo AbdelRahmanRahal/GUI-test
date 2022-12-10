425bcb2ae19f50d54fdfd866d54ff119ab455f83
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-10 19:48:46 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -2,11 +2,12 @@ import tkinter as tkn
 
 # ----- WINDOW SETTINGS -----
 wndw = tkn.Tk()
+wndw.configure(bg="#D5E5ED")
 wndw.title("Test title!")
 wndw.geometry("500x500")
 
 # ----- LABEL TEST -----
-lbl = tkn.Label(master=wndw, text="Hello, world!", fg="red")
+lbl = tkn.Label(master=wndw, text="Hello, world!", fg="red", bg="#D5E5ED")
 lbl.grid()
 
 # ----- PROGRAM INITIATION -----
