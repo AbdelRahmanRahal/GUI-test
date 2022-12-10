b1126e017955f9c4878933b70cced2dc43b74e38
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-10 19:58:58 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -7,7 +7,7 @@ wndw.geometry("500x500")
 wndw.configure(bg="#D5E5ED")
 
 # ----- LABEL TEST -----
-lbl = tkn.Label(master=wndw, text="Hello, world!", fg="#001061", bg="#D5E5ED", font=("Segoe UI", 20))
+lbl = tkn.Label(master=wndw, text="Hello, world!", font=("Segoe UI", 20), fg="#001061", bg="#D5E5ED")
 lbl.grid()
 
 # ----- PROGRAM INITIATION -----
