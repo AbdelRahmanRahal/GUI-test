a5428e376b4582e54fcb22b3a2c96207132e89a4
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-10 21:09:01 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,14 +1,23 @@
-import tkinter as tkn
+from tkinter import *
 
 # ----- WINDOW SETTINGS -----
-wndw = tkn.Tk()
+wndw = Tk()
 wndw.title("Test title!")
-wndw.geometry("500x500")
+wndw.geometry("800x450")
 wndw.configure(bg="#D5E5ED")
 
-# ----- LABEL TEST -----
-lbl = tkn.Label(master=wndw, text="Hello, world!", font=("Segoe UI", 20), fg="#001061", bg="#D5E5ED")
-lbl.grid()
+# ----- TITLE -----
+titlelabel = Label(master=wndw, text="Text Converter\n", font=("Segoe UI", 22, "bold", "underline"), fg="#001061", bg="#D5E5ED")
+titlelabel.grid(column=1)
+
+# ----- BODY -----
+# ----- USERINPUT -----
+boxlabel = Label(master=wndw, text="Enter text to convert:  ", font=("Segoe UI", 16), fg="#001061", bg="#D5E5ED")
+boxlabel.grid(column=0, row=1)
+txtbox = Entry(master=wndw, font=("Segoe UI", 16), fg="#001061", width=35)
+txtbox.grid(column=1, row=1)
+linebreak = Label(master=wndw, text="", bg="#D5E5ED")
+linebreak.grid(column=1, row=2)
 
 # ----- PROGRAM INITIATION -----
 wndw.mainloop()
