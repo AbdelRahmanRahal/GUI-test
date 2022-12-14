2bd8cdc7fc5284980eb937bb404858eaed3ff573
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-14 08:32:08 +0200

    Update Caeser.py

--- Caeser.py
+++ Caeser.py
@@ -1,12 +1,1 @@
-txt = input("Enter your text: ")
-num = int(input("Enter the number of shifts: "))
-encr = ""
-
-chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.?! *$@&"
-
-for i in txt:
-	for q in chars:
-		if i == q:
-
-			encr = encr + chars[chars.find(i) + num]
-print(encr)
+txt 
