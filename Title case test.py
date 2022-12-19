a71776c8d3a45232010590e960eefe3c26d1e30a
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-19 08:12:28 +0200

    Update Title case test.py

--- Title case test.py
+++ Title case test.py
@@ -1,32 +1,25 @@
-# ----- IGNORED WORDS LIST -----
+# ----- DECLARATIONS -----
 ignrdwords = ["and", "as", "but", "for", "if", "nor", "or", "so", "yet", "a", "an", "the", "as", "at", "by", "for", "in", "of", "off", "on", "per", "to", "up", "via"]
 cnt = 0
+
+# ----- FIRST LETTER CAPITALISER -----
+def FrstLttr():
+	inplist[cnt] = inplist[cnt][0].upper() + inplist[cnt][1:]
 
 # ----- USER INPUT -----
 inp = input("Enter a title: ")
 inplist = inp.lower().split()
 
-# ----- FIRST AND LAST WORDS' CASES -----
-# frstword = inplist[0][0].upper() + inplist[0][1:]
-# lstword = inplist[len(inplist) - 1][0].upper() + inplist[len(inplist) - 1][1:]
-inplist[0] = inplist[0][0].upper() + inplist[0][1:]
-inplist[len(inplist) - 1] = inplist[len(inplist) - 1][0].upper() + inplist[len(inplist) - 1][1:]
-# inplist.pop(0)
-# inplist.pop(len(inplist) - 1)
-
-# -----
+# ----- TITLE CASE CONVERTER -----
 for word in inplist:
-	if (word[len(word) - 1] == ":") or (word[len(word) - 1] == "—"):
-		inplist[cnt + 1] = inplist[cnt + 1][0].upper() + inplist[cnt + 1][1:]
-	# elif (cnt > 0) and (cnt < (len(inplist) - 1)):
-	# 	if word not in ignrdwords:
-	# 		inplist[cnt] = word[0].upper() + word[1:]
-	elif not ((word in ignrdwords) and (cnt > 0) and (cnt < (len(inplist) - 1))):
-		inplist[cnt] = word[0].upper() + word[1:]
-	else:
-		pass
-
+	if (cnt == 0) or (cnt == (len(inplist) - 1)):
+		FrstLttr()
+	elif inplist[cnt - 1][len(inplist[cnt - 1]) - 1] in ":—":
+		FrstLttr()
+	elif word not in ignrdwords:
+		FrstLttr()
 	cnt += 1
 
-print(inplist)
-# print(frstword, lstword, inplist)
+# ----- OUTPUT -----
+output = " ".join(inplist)
+print(output)
