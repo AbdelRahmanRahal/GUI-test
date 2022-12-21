a52c232f53f210c2113f374e477cad2c4e25983d
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-21 08:13:36 +0200

    Create ConverterFunctions.py

---
+++ ConverterFunctions.py
@@ -0,0 +1,46 @@
+# ----- Title Case -----
+def Title(txt):
+	# ----- DECLARATIONS -----
+	ignrdwords = ["and", "as", "but", "for", "if", "nor", "or", "so", "yet", "a", "an", "the", "as", "at", "by", "for", "in", "of", "off", "on", "per", "to", "up", "via"]
+	cnt = 0
+
+	# ----- FIRST LETTER CAPITALISER -----
+	def FrstLttr():
+		inplist[cnt] = inplist[cnt][0].upper() + inplist[cnt][1:]
+
+	# ----- USER INPUT -----
+	inplist = txt.lower().split()
+
+	# ----- TITLE CASE CONVERTER -----
+	for word in inplist:
+		if (cnt == 0) or (cnt == (len(inplist) - 1)):
+			FrstLttr()
+		elif inplist[cnt - 1][len(inplist[cnt - 1]) - 1] in ":—":
+			FrstLttr()
+		elif word not in ignrdwords:
+			FrstLttr()
+		cnt += 1
+
+	# ----- OUTPUT -----
+	return " ".join(inplist)
+
+# ----- AlT CaSe -----
+def Alt(txt):
+	alted = ""
+
+	for char in range(len(txt)):
+		if char % 2:
+			alted += txt[char].lower()
+		else:
+			alted += txt[char].upper()
+
+	return alted
+
+# ----- Caesar Cipher -----
+def Caesar(txt, shift):
+	encrypted = ""
+
+	for char in txt:
+		encrypted += chr(ord(char) + shift)
+
+	return encrypted
