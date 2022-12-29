2d1f203c0d51f7d75b5751953c14ad6ef0e240d7
AbdelRahman <abdelrahman.rahalll@gmail.com>
2022-12-29 22:05:34 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -29,6 +29,11 @@ def CaesarButton():
 	dlgbox = simpledialog.askinteger("Caesar Cipher", "Enter number of shifts!", parent=wndw, minvalue=-25, maxvalue=25)
 	outputbox.insert(END, Caesar(txtbox.get(), dlgbox))
 
+# ----- Password Generator -----
+def PasswordButton():
+	outputbox.delete(1.0, END)
+	outputbox.insert(END, PasswordGen(txtbox.get(), PGchckboxVar))
+
 # ----- WINDOW SETTINGS -----
 wndw = Tk()
 wndw.title("Text Converter")
@@ -58,17 +63,29 @@ LCbttn = Button(text="lowercase", font=("Segoe UI", 11), fg="#001061", command=L
 LCbttn.grid(column=3, row=3)
 ACbttn = Button(text="AlT CaSe", font=("Segoe UI", 11), fg="#001061", command=AltButton)
 ACbttn.grid(column=4, row=3)
-CCbttn = Button(text="Caesar Cipher", font=("Segoe UI", 11), fg="#001061", command=CaesarButton)
-CCbttn.grid(column=5, row=3)
+SCbttn = Button(text="Spelling Checker", font=("Segoe UI", 11), fg="#001061", command=CaesarButton)
+SCbttn.grid(column=5, row=3)
 
-linebreak2 = Label(master=wndw, text="\n\n", bg="#D5E5ED")
+linebreak2 = Label(master=wndw, text="", bg="#D5E5ED")
 linebreak2.grid(column= 0, row=4, columnspan=6)
+
+CCbttn = Button(text="Caesar Cipher", font=("Segoe UI", 11), fg="#001061", command=CaesarButton)
+CCbttn.grid(column=1, row=5, columnspan=2)
+PGbttn = Button(text="Password Generator", font=("Segoe UI", 11), fg="#001061", command=PasswordButton)
+PGbttn.grid(column=3, row=5, columnspan=2)
+
+PGchckboxVar = IntVar()
+PGchckbox = Checkbutton(master=wndw, text="Allow Spaces", font=("Segoe UI", 11), fg="#001061", bg="#D5E5ED", activebackground="#C4DDEA", onvalue = 1, offvalue = 0, variable=PGchckboxVar)
+PGchckbox.grid(column=5, row=5)
+
+linebreak3 = Label(master=wndw, text="\n", bg="#D5E5ED")
+linebreak3.grid(column= 0, row=6, columnspan=6)
 
 # ----- OUTPUT -----
 boxlabel2 = Label(master=wndw, text="Converted text:", font=("Segoe UI", 16), fg="#001061", bg="#D5E5ED")
-boxlabel2.grid(column=0, row=5)
+boxlabel2.grid(column=0, row=7)
 outputbox = Text(master=wndw, height=1, width=45, font=("Segoe UI", 16), fg="#001061")
-outputbox.grid(column=1, row=5, columnspan=5)
+outputbox.grid(column=1, row=7, columnspan=5)
 
 # ----- PROGRAM INITIATION -----
 wndw.mainloop()
